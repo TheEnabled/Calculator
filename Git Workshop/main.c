59a7000fc49e26787a6e87b1ead8add06ac8adbf
ReikiRat <rhyss_medina@dlsu.edu.ph>
2025-02-08 16:00:42 +0800

    push it

--- Git Workshop/main.c
+++ Git Workshop/main.c
@@ -7,7 +7,7 @@ void inputnumbers(float *firstnum, float *secondnum)
     printf("Enter first num: ");
     scanf("%f", firstnum);
     printf("Enter second num: ");
-    scnaf("%f", secondnum);
+    scanf("%f", secondnum);
 }
 
 int main()
@@ -15,7 +15,8 @@ int main()
     float firstnum, secondnum, ans;
 
     inputnumbers(&firstnum, &secondnum);
-    ans = Addition(&firstnum, &secondnum);
-
+    ans = Addition(firstnum, secondnum);
+	
+	printf("The answer is: %.2f", ans);
     return 0;
 }
