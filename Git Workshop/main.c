f7e14db908cc8e2b8f552d5e183e8827b92006e6
TheEnabled <sol_tabboga@dlsu.edu.ph>
2025-02-08 15:27:34 +0800

    Added Operations header

--- Git Workshop/main.c
+++ Git Workshop/main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include "operations.h"
 
 int main()
 {
-
+    
 
     return 0;
 }
---
+++ Git Workshop/operations.h
@@ -0,0 +1,15 @@
+int Addition (int *num1, int *num2){
+    return *num1 + *num2;
+}
+
+int Subtraction (int *num1, int *num2){
+    return *num1 - *num2;
+}
+
+int Multiplication (int *num1, int *num2){
+    return *num1 * *num2;
+}
+
+int Division (int *num1, int *num2){
+    return *num1 / *num2;
+}
