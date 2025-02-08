5267dd1d53bab1af50aae49435f65aeb87d9fe86
TheEnabled <sol_tabboga@dlsu.edu.ph>
2025-02-08 15:48:48 +0800

    Fixed Operations

--- Git Workshop/operations.h
+++ Git Workshop/operations.h
@@ -1,15 +1,15 @@
-int Addition (int *num1, int *num2){
-    return *num1 + *num2;
+int Addition (float num1, float num2){
+    return num1 + num2;
 }
 
-int Subtraction (int *num1, int *num2){
-    return *num1 - *num2;
+int Subtraction (float num1, float num2){
+    return num1 - num2;
 }
 
-int Multiplication (int *num1, int *num2){
-    return *num1 * *num2;
+int Multiplication (float num1, float num2){
+    return num1 * num2;
 }
 
-int Division (int *num1, int *num2){
-    return *num1 / *num2;
+int Division (float num1, float num2){
+    return num1 / num2;
 }
