76ba361b6133cac1a41c3644f270f54234c06fb1
TheEnabled <sol_tabboga@dlsu.edu.ph>
2025-02-08 16:13:50 +0800

    Options added final

--- Git Workshop/main.c
+++ Git Workshop/main.c
@@ -15,7 +15,35 @@ int main()
     float firstnum, secondnum, ans;
 
     inputnumbers(&firstnum, &secondnum);
-    ans = Addition(&firstnum, &secondnum);
+
+    printf("1. Addition, 2. Subtraction, 3. Multiplication, 4.Division");
+    printf("What operation will you use: ");
+
+    int choice;
+
+    scanf("%d", choice);
+
+    switch(choice)
+    {
+        case 1:{
+            ans = Addition(firstnum, secondnum);
+            break;
+        }
+        case 2:{
+            ans = Subtraction(firstnum, secondnum);
+            break;
+        }
+        case 3:{
+            ans = Multiplication(firstnum, secondnum);
+            break;
+        }
+        case 4:{
+            ans = Division(firstnum, secondnum);
+            break;
+        }
+    }
+
+    printf("\nThe Final Answer is: %.2f", ans);
 
     return 0;
 }
